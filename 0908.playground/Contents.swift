b04e52dd2e74d4bdf256b4ddd8a6aac186a6fa02
james-shih <jamesshih.me@gmail.com>
2017-09-08 18:41:50 +0800

    0908

---
+++ 0908.playground/Contents.swift
@@ -0,0 +1,30 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//The Baiscs
+
+let maximumNumberOfLoginAttempts = 10
+var currentLoginAttempt = 0
+
+var x = 0.0, y = 0.0, z = 0.0
+
+var welcomeMessage: String
+welcomeMessage = "Hello"
+
+var red, green, blue: Double
+
+//Naming Constants and Variables
+let pi = 3.14159
+let 你好 = "你好世界"
+let 🐶🐮 = "dogcow"
+
+var friendlyWelcome = "Hello!"
+friendlyWelcome = "Bonjour!"
+
+let languageName = "Swift"
+//languageName = "Swift++"
+print(friendlyWelcome)
+print("The current value of friendlyWelcome is \(friendlyWelcome)")
+
+
