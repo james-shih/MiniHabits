e6635ae92e864047fbd9ad664e84fa12b5b10657
james-shih <jamesshih.me@gmail.com>
2017-09-09 17:22:47 +0800

    0909

---
+++ 0909.playground/Contents.swift
@@ -0,0 +1,30 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Integers
+
+let minValue = UInt8.min
+let maxValue = UInt8.max
+
+//Type Safety and Type Inference
+
+let meaningOfLife = 42
+let pi = 3.14159
+
+let anotherPi = 3 + 0.14159
+
+//Numeric Literals
+
+let decimalInteger = 17
+let binaryInteger = 0b10001
+let obtalInteger = 0o21
+let hexadecimalInteger = 0x11
+
+let decimalDouble = 12.1875
+let exponentDouble = 1.21875e1
+let hexadecimalDouble = 0xC.3p0
+
+let paddedDouble = 000123.456
+let oneMillion = 1_000_000
+let justOverOneMillion = 1_000_000.000_000_1
