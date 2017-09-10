40eeb9259ff3fce03fc80d5708f3efc992392c06
james-shih <jamesshih.me@gmail.com>
2017-09-10 09:10:48 +0800

    0910

---
+++ 0910.playground/Contents.swift
@@ -0,0 +1,61 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Numberic Type Conversion
+
+//let cannotBeNegative: UInt8 = -1
+//let tooBig: Int8 = Int8.max + 1
+
+let twoThousand: UInt16 = 2_000
+let one: UInt8 = 1
+let twoThousandAndOne = twoThousand + UInt16(one)
+
+let three = 3
+let pointOneFourOneFiveNine = 0.14159
+let pi = Double(three) + pointOneFourOneFiveNine
+
+let integerPi = Int(pi)
+
+//Type Aliases
+
+typealias AudioSample = UInt16
+var maxAmplitudeFound = AudioSample.min
+
+//Booleans
+
+let orangeAreOrange = true
+let turnipsAreDelicious = false
+
+if turnipsAreDelicious {
+    print("Mmm, tasty turnips!")
+} else {
+    print("Eww, turnips are horrible.")
+}
+
+let i = 1
+//if i {
+//    
+//}
+
+if i == 1 {
+    
+}
+
+//Tuples
+
+let http404Error = (404, "Not Found")
+let (statusCode, statusMessage) = http404Error
+print("The status code is \(statusCode)")
+print("The status message is \(statusMessage)")
+
+let (justTheStatusCode, _) = http404Error
+print("The status code is \(justTheStatusCode)")
+
+print("The status code is \(http404Error.0)")
+print("The status message is \(http404Error.1)")
+
+let http200Status = (statusCode: 200, description: "OK")
+print("The status code is \(http200Status.statusCode)")
+print("The status message is \(http200Status.description)")
+
