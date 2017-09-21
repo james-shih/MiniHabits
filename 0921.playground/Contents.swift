1e328da7a240765138bd0fa656131ffaa15a12be
james-shih <jamesshih.me@gmail.com>
2017-09-21 16:14:09 +0800

    0921

---
+++ 0921.playground/Contents.swift
@@ -0,0 +1,60 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Dictionary
+
+//Creating an Empty Dictionary
+var namesOfIntegers = [Int: String]()
+
+namesOfIntegers[16] = "sixteen"
+namesOfIntegers = [:]
+
+//Creating a Dictionary with a Dictionary Literal
+//var airports: [String: String] = ["YYZ": "Toronto Pearson", "DUB": "Dublin"]
+var airports = ["YYZ": "Toronto Pearson", "DUB": "Dublin"]
+
+//Accessing and Mofifying a Dictionary
+print("The airports dictionary contains \(airports.count) items.")
+
+if airports.isEmpty {
+    print("The airports dictionary is empty.")
+} else {
+    print("The airports dictionary is not empty.")
+}
+
+airports["LHR"] = "London"
+airports["LHR"] = "London Heathrow"
+
+if let oldValue = airports.updateValue("Dublin Airport", forKey: "DUB") {
+    print("The old value for DUB was \(oldValue).")
+}
+
+airports["APL"] = "Apple International"
+airports["APL"] = nil
+
+if let removedValue = airports.removeValue(forKey: "DUB") {
+    print("The removed airport's name is \(removedValue).")
+} else {
+    print("The airports dictionary does not contain a value for DUB.")
+}
+
+//Iterating Over a Dictionary
+for (airportCode, airportName) in airports {
+    print("\(airports): \(airportName)")
+}
+
+for airportCode in airports.keys {
+    print("Airport code: \(airportCode)")
+}
+
+for airportName in airports.values {
+    print("Airport name: \(airportName)")
+}
+
+let airportCodes = [String](airports.keys)
+let airportNames = [String](airports.values)
+
+
+
+
