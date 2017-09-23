c6cb306c18c92abfab357abe9f4d9df7df0351a1
james-shih <jamesshih.me@gmail.com>
2017-09-23 14:35:11 +0800

    0923

---
+++ 0923.playground/Contents.swift
@@ -0,0 +1,36 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//While Loops
+
+let finalSquare = 25
+var board = [Int](repeating: 0, count: finalSquare + 1)
+
+board[03] = +08; board[06] = +11; board[09] = +09; board[10] = +02
+board[14] = -10; board[19] = -11; board[22] = -02; board[24] = -08
+
+var square = 0
+var diceRoll = 0
+
+while square < finalSquare {
+    diceRoll += 1
+    if diceRoll == 7 { diceRoll = 1}
+    square += diceRoll
+    if square < board.count {
+        square += board[square]
+    }
+}
+
+print("Game over")
+
+//Repeat-While
+
+repeat {
+    square += board[square]
+    diceRoll += 1
+    if diceRoll == 7 {diceRoll = 1}
+    square += diceRoll
+} while square < finalSquare
+
+print("Game over!")
