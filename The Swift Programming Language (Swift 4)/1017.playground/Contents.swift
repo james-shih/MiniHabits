2af13ee785f8d05dfd694edbe4dddfc0bc98eda3
James Shih <jamesshih.me@gmail.com>
2017-10-18 23:27:56 +0800

    1018

--- The Swift Programming Language (Swift 4)/1017.playground/Contents.swift
+++ The Swift Programming Language (Swift 4)/1017.playground/Contents.swift
@@ -80,6 +80,95 @@ enum OnOffSwitch: Togglable {
 var lightSwitch = OnOffSwitch.off
 lightSwitch.toggle()
 
+//Initializer Requirements
+
+//Protocols as Types
+class Dice {
+    let sides: Int
+    let generator: RandomNumberGenerator
+    init(sides: Int, generator: RandomNumberGenerator) {
+        self.sides = sides
+        self.generator = generator
+    }
+    
+    func roll() -> Int {
+        return Int(generator.random() * Double(sides)) + 1
+    }
+}
+
+var d6 = Dice(sides: 6, generator: LinearCongruentialGenerator())
+for _ in 1...5 {
+    print("Random dice roll is \(d6.roll())")
+}
+
+//Delegation
+protocol DiceGame {
+    var dice: Dice { get }
+    func play()
+}
+
+protocol DiceGameDelegate {
+    func gameDidStart(_ game: DiceGame)
+    func game(_ game: DiceGame, didStartNewTurnWithDiceRoll diceRoll: Int)
+    func gameDidEnd(_ game: DiceGame)
+}
+
+class SnakeAndLadders: DiceGame {
+    let finalSquere = 25
+    let dice = Dice(sides: 6, generator: LinearCongruentialGenerator())
+    var squre = 0
+    var board: [Int]
+    init() {
+        board = Array(repeatElement(0, count: finalSquere + 1))
+        board[03] = +08; board[06] = +11; board[09] = +09; board[10] = +02; board[14] = -10; board[19] = -11; board[22] = -02; board[24] = -08
+    }
+    var delegate: DiceGameDelegate?
+    func play() {
+        squre = 0
+        delegate?.gameDidStart(self)
+        gameLoop: while squre != finalSquere {
+            let diceRoll = dice.roll()
+            delegate?.game(self, didStartNewTurnWithDiceRoll: diceRoll)
+            switch squre + diceRoll {
+            case finalSquere:
+                break gameLoop
+            case let newSquare where newSquare > finalSquere:
+                continue gameLoop
+            default:
+                squre += diceRoll
+                squre += board[squre]
+            }
+        }
+        delegate?.gameDidEnd(self)
+    }
+}
+
+class DiceGameTraker: DiceGameDelegate {
+    var numberOfTurns = 0
+    
+    func gameDidStart(_ game: DiceGame) {
+        numberOfTurns = 0
+        if game is SnakeAndLadders {
+            print("Started a new game of Snakes and Ladders")
+        }
+        print("The game is using a \(game.dice.sides)-sided dice")
+    }
+    
+    func game(_ game: DiceGame, didStartNewTurnWithDiceRoll diceRoll: Int) {
+        numberOfTurns += 1
+        print("Rolled a \(diceRoll)")
+    }
+    
+    func gameDidEnd(_ game: DiceGame) {
+        print("The game lasted for \(numberOfTurns) turns")
+    }
+}
+
+let tracker = DiceGameTraker()
+let game = SnakeAndLadders()
+game.delegate = tracker
+game.play()
+
 
 
 
