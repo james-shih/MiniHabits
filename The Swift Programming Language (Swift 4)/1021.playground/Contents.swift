d06ae0734cf9435c58544d3118ea97d638abaca6
James Shih <jamesshih.me@gmail.com>
2017-10-24 16:44:16 +0800

    1024

--- The Swift Programming Language (Swift 4)/1021.playground/Contents.swift
+++ The Swift Programming Language (Swift 4)/1021.playground/Contents.swift
@@ -126,36 +126,149 @@ protocol Container {
 
 struct InsStack: Container {
     var items = [Int]()
-    
+
     mutating func push(_ item: Int) {
         items.append(item)
     }
-    
+
     mutating func pop() -> Int {
         return items.removeLast()
     }
-    
+
     typealias Item = Int
-    
+
     mutating func apped(_ item: Int) {
         self.push(item)
     }
-    
+
     var count: Int {
         return items.count
     }
-    
+
     subscript(i: Int) -> Int {
         return items[i]
     }
 }
 
+struct Stack1<Element>: Container {
+    var items = [Element]()
+    mutating func push(_ item: Element) {
+        items.append(item)
+    }
+    mutating func pop() -> Element {
+        return items.removeLast()
+    }
+    mutating func apped(_ item: Stack<Element>.Item) {
+        self.push(item)
+    }
+    var count: Int {
+        return items.count
+    }
+    subscript(i: Int) -> Element {
+        return items[i]
+    }
+}
 
+//Extending an Existing Type to Specify an Associated Type
+extension Array: Container {}
 
+//Using Type Annnotations to Constrain an Associated Type
 
+//Geneice Where Clauses
+func allItemsMatch<C1: Container, C2: Container>(_ someContainer: C1, _ anotherContainer: C2) -> Bool where C1.Item == C2.Item, C1.Item: Equatable {
+    if someContainer.count != anotherContainer.count {
+        return false
+    }
 
+    for i in 0..<someContainer.count {
+        if someContainer[i] != anotherContainer[i] {
+            return false
+        }
+    }
+    return true
+}
 
+var stackOfStrings = Stack<String>()
+stackOfStrings.push("uno")
+stackOfStrings.push("dos")
+stackOfStrings.push("tres")
 
+var arrayOfStrings = ["uno", "dos", "tres"]
+
+if allItemsMatch(stackOfStrings, arrayOfStrings) {
+    print("All items match.")
+} else {
+    print("Not all items match.")
+}
+
+//Extensions with a Generic Where Clause
+extension Stack1 where Element: Equatable {
+    func isTop(_ item: Element) -> Bool {
+        guard let topItem = items.last else {
+            return false
+        }
+        return topItem == item
+    }
+}
+
+if stackOfStrings.isTop("tres") {
+    print("Top element is tres.")
+} else {
+    print("Top element is something else.")
+}
+
+struct NotEquatable { }
+var notEquatableStack = Stack1<NotEquatable>()
+let notEquatableValue = NotEquatable()
+notEquatableStack.push(notEquatableValue)
+
+extension Container where Item: Equatable {
+    func startsWith(_ item: Item) -> Bool {
+        return count >= 1 && self[0] == item
+    }
+}
+
+if [9, 9, 9].startIndex(42) {
+    print("Starts with 42.")
+} else {
+    print("Starts with something else")
+}
+
+extension Container where Item == Double {
+    func average() -> Double {
+        var sum = 0.0
+        for index in 0..<count {
+            sum += self[index]
+        }
+        return sum / Double(count)
+    }
+}
+
+print([1260.0, 1200.0, 98.6, 37.0].average())
+
+//Associated Types a Generic Where Clause
+protocol Container1 {
+    associatedtype Item
+    mutating func append(_ item: Item)
+    var count: Int { get }
+    subscript(i: Int) -> Item { get }
+    
+    associatedtype Iterator: IteratorProtocol where Iterator.Element == Item
+    func makeIterator() -> Iterator
+}
+
+protocol ComparableContainer: Container: where Item: Comparable { }
+
+//Generic Subscripts
+extension Container {
+    subscript<Indices: Sequence>(indices: Indices) -> [Item] where Indices.Iterator.Element == Int {
+        var result = [Item]()
+        for index in indices {
+            result.append(self[index])
+        }
+        return result
+    }
+}
 
 
 
