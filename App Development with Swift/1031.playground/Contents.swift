41d9336ec0710480529d71814caaf825bd5993eb
James Shih <jamesshih.me@gmail.com>
2017-10-31 16:14:19 +0800

    1031

---
+++ App Development with Swift/1031.playground/Contents.swift
@@ -0,0 +1,49 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Operators
+//Assign a Value
+let favoritePerson = "Luke"
+var shoeSize = 8
+shoeSize = 9
+
+//Basic Arithmetic
+var opponentScore = 3 * 8
+var myScore = 100 / 4
+
+var totalScore = opponentScore + myScore
+myScore = myScore + 3
+
+let totolDistance = 3.9
+var distanceTravelled = 1.2
+var remainingDistance = totolDistance - distanceTravelled
+
+let x: Double = 51
+let y: Double = 4
+let z = x / y
+
+//Compound Assignment
+myScore += 3
+myScore -= 5
+myScore *= 2
+myScore /= 2
+
+//Order of Operators
+var a = 2
+var b = 3
+var c = 5
+//a + b * c
+(a + b) * c
+
+//Numberic Type Conversion
+let x1 = 3
+let y1 = 0.1415927
+let pi = Double(x1) + y1
+
+
+
+
+
+
+
